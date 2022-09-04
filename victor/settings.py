4a7eba669e11583c7ba5ec1ffdd70641a68b2818
citizenccc <v.pisarev69@gmail.com>
2022-09-03 21:24:46 -0400

    victor_2

--- victor/settings.py
+++ victor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ywf7yoe3-s96j9s^33vn4)rgwapo3xzp7s$*pm^o1o^6&0kzo$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
